f2b2772d5cd06c68581a80a74b2cad789dbe0015
jjolisrp <165792842+jjolisrp@users.noreply.github.com>
2024-11-04 16:08:07 +0100

    Improving menu fader script

--- Assets/MenuSection/Scripts/MenuFader.cs
+++ Assets/MenuSection/Scripts/MenuFader.cs
@@ -2,12 +2,16 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class MenuFader : MonoBehaviour
 {
     [SerializeField] bool startsFadedOut;
+    [SerializeField] GameObject startSelection;
+
     CanvasGroup canvasGroup;
+    private EventSystem eventSystem;
 
     void Awake()
     {
@@ -21,11 +25,19 @@ public class MenuFader : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        eventSystem = EventSystem.current;
+    }
+
     public void FadeIn()
     {
         canvasGroup.blocksRaycasts = true;
         canvasGroup.interactable = true;
         canvasGroup.DOFade(1f, 0.5f).SetEase(Ease.Linear);
+
+        eventSystem.SetSelectedGameObject(null);
+        eventSystem.SetSelectedGameObject(startSelection);
     }
 
     public void FadeOut()
