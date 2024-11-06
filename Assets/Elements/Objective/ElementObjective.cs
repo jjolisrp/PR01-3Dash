681cec0866f6ad0a7d00818e7866434cc66924f1
jjolisrp <165792842+jjolisrp@users.noreply.github.com>
2024-11-06 19:30:55 +0100

    Improved objectives from the special sction

--- Assets/Elements/Objective/ElementObjective.cs
+++ Assets/Elements/Objective/ElementObjective.cs
@@ -5,18 +5,39 @@ using UnityEngine;
 public class ElementObjective : ElementsManager
 {
     [SerializeField] ElementObjective secondStar;
+    [SerializeField] GameObject objectiveWall;
+    
     bool playerTouch;
+    bool canPassWall;
+
+    private void OnEnable()
+    {
+        PlayerController.PlayerRestarted += RestartWall;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         playerTouch = false;
+        canPassWall = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(canPassWall)
+        {
+            objectiveWall.SetActive(false);
+        }
+        else if(!canPassWall)
+        {
+            objectiveWall.SetActive(true);
+        }
+    }
+
+    void RestartWall()
+    {
+        canPassWall = false;
     }
 
     protected override void ApplyEffect(PlayerController player)
@@ -24,7 +45,13 @@ public class ElementObjective : ElementsManager
         playerTouch = true;
         if(secondStar.playerTouch == false)
         {
-            player.KillPlayer();
+            //player.KillPlayer();
+            canPassWall = true;
         }
+    }
+
+    private void OnDisable()
+    {
+        PlayerController.PlayerRestarted -= RestartWall;
     }
 }
