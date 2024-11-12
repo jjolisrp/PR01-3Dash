4e5215028e8138d026092b090d21bc02737ae809
jjolisrp <165792842+jjolisrp@users.noreply.github.com>
2024-11-12 10:16:38 +0100

    Pants

---
+++ Assets/MenuSection/FinalLevel/FinalLevel.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalLevel : MonoBehaviour
+{
+    [SerializeField] Button restartButton;
+    [SerializeField] Button mainMenuButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
