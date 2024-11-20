71d4f06345b73d67920ab2cf2fe107468e7974ac
jjolisrp <165792842+jjolisrp@users.noreply.github.com>
2024-11-20 21:52:56 +0100

    Jump sound effect for player

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -19,6 +19,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] InputActionReference scaleY;
     [SerializeField] InputActionReference jump;
 
+    [Header("Sound Effects")]
+    [SerializeField] AudioSource jumpEffect;
+
     private Rigidbody playerRb;
     private BoxCollider[] playerColliders;
 
@@ -110,6 +113,8 @@ public class PlayerController : MonoBehaviour
                 newVelocity.y = jumpSpeed;
                 particlesManager.EmitParticles(70, transform, ParticlesManager.Particle.PlayerJump);
                 playerRb.velocity = newVelocity;
+
+                jumpEffect.Play();
 
                 isGrounded = false;
             }
