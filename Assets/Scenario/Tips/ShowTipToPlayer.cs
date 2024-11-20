6f2306b5ae5c348ea06ee2ad2f6e50cec407691f
jjolisrp <165792842+jjolisrp@users.noreply.github.com>
2024-11-20 19:32:54 +0100

    Solved  chema bug 7

---
+++ Assets/Scenario/Tips/ShowTipToPlayer.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShowTipToPlayer : MonoBehaviour
+{
+    [SerializeField] GameManager gameManager;
+
+    TMP_Text tipText;
+    GameObject background;
+
+    bool isActive;
+    bool keyWasAlreadyPressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tipText = GetComponent<TMP_Text>();
+        background = transform.GetChild(0).gameObject;
+
+        tipText.enabled = false;
+        background.SetActive(false);
+        keyWasAlreadyPressed = true;
+        isActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && !keyWasAlreadyPressed)
+        {
+            keyWasAlreadyPressed = true;
+            DespauseGame();
+        }
+    }
+
+    void ShowTipAnStopGame()
+    {
+        Time.timeScale = 0f;
+        gameManager.GamePause();
+
+        tipText.enabled = true;
+        background.SetActive(true);
+    }
+
+    void DespauseGame()
+    {
+        Time.timeScale = 1f;
+        gameManager.GamePause();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == 6)
+        {
+            PlayerController player = other.GetComponent<PlayerController>();
+
+            if(player != null && !isActive)
+            {
+                isActive = true;
+                keyWasAlreadyPressed = false;
+                ShowTipAnStopGame();
+            }
+        }
+    }
+}
