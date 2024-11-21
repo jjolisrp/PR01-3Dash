6c2cf5bf065ae15663bcc9b3529fbae05515ad85
jjolisrp <165792842+jjolisrp@users.noreply.github.com>
2024-11-21 21:31:10 +0100

    Added load scene asyn from m17 project

---
+++ Assets/LoadingScene/Scripts/LoadingIcon.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class LoadingIcon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DORotate(Vector3.forward * 360f, 3f, RotateMode.FastBeyond360)
+            .SetEase(Ease.OutBounce)
+            .SetLoops(-1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/LoadingScene/Scripts/LoadingScene.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class LoadingScene : MonoBehaviour
+{
+    [SerializeField] CanvasGroup canvasGroup;
+    Scene currentScene;
+
+    static public LoadingScene instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine(LoadSceneCorutine(sceneName));
+    }
+
+    public IEnumerator LoadSceneCorutine(string sceneName)
+    {
+        //Fade
+        {
+            Tween fadeTween = canvasGroup.DOFade(1f, 1f);
+            do
+            {
+                yield return new();
+            }
+            while (fadeTween.IsPlaying());
+        }
+
+        //Descargar la escena actual
+        if(currentScene.isLoaded)
+        {
+            AsyncOperation unloadOperation = SceneManager.UnloadSceneAsync(currentScene);
+            do
+            {
+                yield return new();
+            }
+            while (!unloadOperation.isDone);
+        }
+
+        float timeBeforeLoad = Time.realtimeSinceStartup;
+        //Cargar la escena
+        {
+            AsyncOperation loadOperation = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+            do
+            {
+                yield return new();
+            }
+            while (!loadOperation.isDone);
+
+            currentScene = SceneManager.GetSceneAt(1);
+            SceneManager.SetActiveScene(currentScene);
+        }
+
+        float timeElapsedLoading = Time.realtimeSinceStartup - timeBeforeLoad;
+        if(timeElapsedLoading < 3f)
+        {
+            yield return new WaitForSeconds(3f - timeElapsedLoading);
+        }
+
+        //Fade
+        {
+            Tween fadeTween = canvasGroup.DOFade(0f, 1f);
+            do
+            {
+                yield return new();
+            }
+            while (fadeTween.IsPlaying());
+        }
+    }
+    [MenuItem("LoadingScene/Debug/Change to MainMenu")]
+    static public void DebugChangeToMainMenuScene()
+    {
+        LoadingScene.instance.LoadScene("MainMenu");
+    }
+}
